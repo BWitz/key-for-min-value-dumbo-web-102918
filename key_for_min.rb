59423bf870b50c9aa76e7ff95cdb9e10f82e58e5
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-16 07:42:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  minuend = 501
+  minuend = 505
   if name_hash.length == 0
     return nil
   end
   name_hash.collect do |key, value|
     new_key = minuend - value
-      if new_key > 0 || new_key > 500
+      if new_key > 0 || new_key > 503
         return key
       else
         return nil

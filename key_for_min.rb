ab4f9341e768b84d6a7f91a27ef390d5f1bef377
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-16 07:35:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,8 @@ def key_for_min_value(name_hash)
   if name_hash.length = 0
     return nil
   end
- name_hash.collect do |key, value|
+  name_hash.collect do |key, value|
+    
       
       
       
